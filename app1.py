957ee989660ed937c1167be8b13e247e205f8651
sowrab9949 <65098918+sowrab9949@users.noreply.github.com>
2020-10-09 18:45:48 +0530

    Create app1.py

---
+++ app1.py
@@ -0,0 +1,26 @@
+# Importing essential libraries
+from flask import Flask, render_template, request
+import pickle
+
+# Load the Multinomial Naive Bayes model and CountVectorizer object from disk
+filename = 'restaurant-sentiment-mnb-model.pkl'
+text_model = pickle.load(open(filename, 'rb'))
+tfidf = pickle.load(open('tfidf-transform.pkl','rb'))
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+	return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    if request.method == 'POST':
+    	message = request.form['message']
+    	data = [message]
+    	vect = tfidf.transform(data).toarray()
+    	my_prediction = text_model.predict(vect)
+    	return render_template('result.html', prediction=my_prediction)
+
+if __name__ == '__main__':
+	app.run(debug=True)
